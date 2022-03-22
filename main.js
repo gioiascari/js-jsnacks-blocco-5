3c4cdb04e10f1aee142b93812bb70a1aeed8996c
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-22 15:24:34 +0100

    inserimento prima traccia nel file js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 console.log("Ok Js :)");
+//Crea un array di 10 oggetti che rappresentano una zucchina,
+//indicando per ognuna varietà, peso e lunghezza.
+//Calcola quanto pesano tutte le zucchine.
