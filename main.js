ca1cd94fe458fa4fcadf7e4e638c74759e3cac9e
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-22 15:34:33 +0100

    create le 10 varietà di oggetti

--- main.js
+++ main.js
@@ -2,3 +2,56 @@ console.log("Ok Js :)");
 //Crea un array di 10 oggetti che rappresentano una zucchina,
 //indicando per ognuna varietà, peso e lunghezza.
 //Calcola quanto pesano tutte le zucchine.
+
+const cassetta = [
+  {
+    varietà: "zucchina napoletana",
+    peso: "50g",
+    lunghezza: "20cm",
+  },
+  {
+    varietà: "zucchina nero di Milano",
+    peso: "75g",
+    lunghezza: "21cm",
+  },
+  {
+    varietà: "zucchina romanesco",
+    peso: "65g",
+    lunghezza: "22cm",
+  },
+  {
+    varietà: "zucchina ortolano di Faenza",
+    peso: "55g",
+    lunghezza: "23cm",
+  },
+  {
+    varietà: "zucchina lunga fiorentina",
+    peso: "100g",
+    lunghezza: "19cm",
+  },
+  {
+    varietà: "zucchina siciliana",
+    peso: "90g",
+    lunghezza: "18cm",
+  },
+  {
+    varietà: "zucchina striata di Napoli",
+    peso: "80g",
+    lunghezza: "15cm",
+  },
+  {
+    varietà: "zucchina bianca triestina",
+    peso: "70g",
+    lunghezza: "17cm",
+  },
+  {
+    varietà: "zucchina rigata pugliese",
+    peso: "60g",
+    lunghezza: "16cm",
+  },
+  {
+    varietà: "zucchina pâtisson",
+    peso: "100g",
+    lunghezza: "19cm",
+  },
+];
